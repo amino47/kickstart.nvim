79e8af55769aa98210cde3f6dc872b6ac0016a71
Amina Shellman <aminashellman@gmail.com>
2024-06-15 08:18:18 -0700

    Changes to be committed: 	modified:   init.lua

--- init.lua
+++ init.lua
@@ -267,6 +267,7 @@ require('lazy').setup({
       require('nvim-tree').setup {}
     end,
   },
+
   {
     'tpope/vim-fugitive',
   },
@@ -281,7 +282,26 @@ require('lazy').setup({
     end,
     dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   },
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup {
+        org_agenda_files = 'C:/Users/Aminas/orgfiles/**/*',
+        org_default_notes_file = 'C:/Users/Aminas/orgfiles/refile.org',
+        org_todo_keywords = {'TODO(t)', '|', 'DONE(d)'},
+      }
 
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
