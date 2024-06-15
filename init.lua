cd708252140008aca3ed6ec75494d54f1351c110
Amina <aminashellman@gmail.com>
2024-06-14 21:07:36 -0400

    Changes to be committed: 	modified:   init.lua

--- init.lua
+++ init.lua
@@ -281,18 +281,6 @@ require('lazy').setup({
     end,
     dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   },
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
-  },
-  {
-    'nvim-neorg/neorg',
-    dependencies = { 'luarocks.nvim' },
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = '*', -- Pin Neorg to the latest stable release
-    config = true,
-  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
