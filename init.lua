49378ed7aa28f19ce7807fd8780895e318f07eaa
Amina shellman <aminashellman@gmail.com>
2025-06-28 16:21:51 -0400

    add rust lsp support

--- init.lua
+++ init.lua
@@ -230,6 +230,12 @@ require('lazy').setup({
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
+  {
+  'mrcjkb/rustaceanvim',
+  version = '^6', -- Recommended
+  lazy = false, -- This plugin is already lazy
+  }
+
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
